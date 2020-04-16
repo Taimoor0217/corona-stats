0cbb360d04f5c02f4fdcf9ebfe4ad39592fc7039
Taimoor Ali <taimoor0217@gmail.com>
2020-04-16 19:30:08 +0500

    update-styles

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -2,7 +2,19 @@ import React from 'react';
 export default function Card(params){
     return(
         <div className="card">
-            <h1>{params.Country} {params.CountryCode}</h1>
+            {/* <h1>{params.Country} {params.CountryCode}</h1> */}
+            <div className="Header"> 
+                <img src={`https://www.countryflags.io/${params.CountryCode}/shiny/64.png`} />
+                <h4> {params.Country} </h4>
+            </div>
+            <div className="Body">
+                <h6 className="statsText">New Cases : {params.NewConfirmed}</h6>
+                <h6 className="statsText">New Death : {params.NewDeaths}</h6>
+                <h6 className="statsText">New Recovered : {params.NewRecovered}</h6>
+                <h6 className="statsText">Total Cases : {params.TotalConfirmed}</h6>
+                <h6 className="statsText">Total Deaths : {params.TotalDeaths}</h6>
+                <h6 className="statsText">New Recovered : {params.TotalRecovered}</h6>
+            </div>
         </div>
     )
 }
